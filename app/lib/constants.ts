b6525204057d08bce3bf6ac48303a768e5f55b07
YamiShots <info.yamishots@gmail.com>
2026-05-28 16:32:16 +0200

    Delete app/lib/constants.ts

--- app/lib/constants.ts
+++
@@ -1,3 +0,0 @@
-export const WA_NUMBER = '393807590956'
-const WA_MESSAGE = 'Ciao, vorrei saperne di più sui servizi YamiShots'
-export const WA_LINK = `https://wa.me/${WA_NUMBER}?text=${encodeURIComponent(WA_MESSAGE)}`
